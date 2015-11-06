83a0db455a239050131e8acaf64a62cf6bb22c28
Aqeel Gunja <desiguy421@gmail.com>
2015-11-05 23:54:29 -0500

    Initial Commit

---
+++ WalmartDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WalmartDemo
+//
+//  Created by Aqeel Gunja on 11/5/15.
+//  Copyright © 2015 Aqeel Gunja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
